0787486fe9af36f95a1b8f110280a8ebc5ef3336
Vojtech Jelinek <vojtech.jelinekk@gmail.com>
2019-10-23 09:02:53 +0200

    reseni uloha 1

--- task01.cpp
+++ task01.cpp
@@ -1,7 +1,21 @@
 #include <cstdio>
+#include <string.h>
+
+char s[100];
 
 const char* toUpper(const char* input) {
-	// TO DO
+	int i;
+	
+	for (i = 0; *input!='\0'; i++) {
+	   if(*input >= 'a' && s[i] <= 'z') {
+       s[i] = *input - 32;     
+     }
+     else {
+       s[i] = *input;
+     } 
+     input++;
+  }
+  return s;
 }
 
 int main() {
