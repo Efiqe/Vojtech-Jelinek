19d170d657e95737ad04b7e416dd404feadfb3cd
Vojtech Jelinek <vojtechjelinekk@gmail.com>
2020-02-05 23:08:09 +0100

    Hotovo program weboveho obchodu

--- task05.cpp
+++ task05.cpp
@@ -3,3 +3,43 @@
   Napište její hardwarovou implementaci pro PC jménem ButtonPC (waitForButton bude čekat na stisk enter a hello bude vypisovat na konzoli).
   Dobře navržený kód umožní použití třídy Button na Arduinu (waitForButton bude čekat na stisk připojeného tlačítka a hello bude vypisovat na Sériovou linku). Tuto část si demonstrujeme spolu při hodině.
 */
+
+#include <cstdio>
+#include <cstring>
+
+struct Button {
+	virtual void waitForButton() = 0;
+	virtual void hello() = 0;
+	void printHello() {
+		while(true) {
+			waitForButton();
+			hello();
+		}
+	}
+};
+
+struct ButtonPC : Button {
+	virtual void waitForButton() {
+		while(getchar() != '\n');
+	}
+	virtual void hello() {
+		puts("hello");
+	}
+	
+};
+
+#include <conio.h>
+struct ButtonWin : ButtonPC {
+	virtual void waitForButton() {
+		getch();
+	} 	
+};
+
+int main() {
+	ButtonPC button;
+	button.printHello();
+}
+
+
+
+
